4f0499d8ea6f0f5e9cbe6e579ac28b6cbe7f90c0
hinshiba <105423175+hinshiba@users.noreply.github.com>
2026-02-12 00:04:49 +0900

    clean: R形式命令の実体の移行

--- PlusPim/Debuggers/PlusPimDbg/Instructions/RType/SubInstruction.cs
+++ PlusPim/Debuggers/PlusPimDbg/Instructions/RType/SubInstruction.cs
@@ -3,9 +3,9 @@ using System.Diagnostics.CodeAnalysis;
 namespace PlusPim.Debuggers.PlusPimDbg.Instructions.RType;
 
 internal sealed class SubInstruction(RegisterID rd, RegisterID rs, RegisterID rt): RTypeInstruction(rd, rs, rt) {
-    public override void Execute(IExecutionContext context) {
-        int rsVal = this.ReadRs(context);
-        int rtVal = this.ReadRt(context);
+    public override void Execute(ExecuteContext context) {
+        int rsVal = context.Registers[this.Rs];
+        int rtVal = context.Registers[this.Rt];
         int result = rsVal - rtVal;
         this.WriteRd(context, result);
         context.Log($"sub ${this.Rd}, ${this.Rs}, ${this.Rt}: 0x{rsVal:X8} - 0x{rtVal:X8} = 0x{result:X8}");
