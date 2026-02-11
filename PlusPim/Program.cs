3911807bf7b797ac21b78d9729b428ffd8f98160
hinshiba <105423175+hinshiba@users.noreply.github.com>
2026-02-12 00:20:03 +0900

    clean: dbg呼出しタイミングの調整によるNullチェックの不要化

--- PlusPim/Program.cs
+++ PlusPim/Program.cs
@@ -1,14 +1,10 @@
-using PlusPim.Debuggers.PlusPimDbg;
 using PlusPim.EditorController.DebugAdapter;
 
 namespace PlusPim;
 
 internal class Program {
     private static void Main(string[] args) {
-        // 各種クラスを呼び出す
-        PlusPimDbg plusPimDbg = new();
-        Application.Application app = new(plusPimDbg);
+        Application.Application app = new();
         _ = new DebugAdapter(Console.OpenStandardInput(), Console.OpenStandardOutput(), app);
-
     }
 }
