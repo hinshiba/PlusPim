8e657497c9425bb05e5069e1c0000fc78f30dde6
hinshiba <105423175+hinshiba@users.noreply.github.com>
2026-02-11 22:49:29 +0900

    add: レジスタファイルのクラス化

---
+++ PlusPim/Debuggers/PlusPimDbg/RegisterFile.cs
@@ -0,0 +1,39 @@
+namespace PlusPim.Debuggers.PlusPimDbg;
+
+/// <summary>
+/// 32本のMIPSの汎用レジスタを管理する
+/// </summary>
+internal sealed class RegisterFile {
+    private readonly int[] _values = new int[32];
+
+    /// <summary>
+    /// 簡単にアクセスするためのインデクサ
+    /// </summary>
+    /// <param name="id">レジスタ番号<see cref="RegisterID"/>を参照すること</param>
+    /// <returns>そのレジスタの値</returns>
+    /// <remarks>$zeroへの書き込みは無視される．
+    /// $zeroに書き込んでもエミュレータで例外等は発生しない</remarks>
+    public int this[RegisterID id] {
+        get => this._values[(int)id];
+        set {
+            // $zero保護: 書き込みを無視
+            if(id == RegisterID.Zero) {
+                return;
+            }
+            this._values[(int)id] = value;
+        }
+    }
+
+    public RegisterFile Clone() {
+        RegisterFile clone = new();
+        Array.Copy(this._values, clone._values, 32);
+        return clone;
+    }
+
+    /// <summary>
+    /// 受け渡し等のために配列化する
+    /// </summary>
+    public int[] ToArray() {
+        return (int[])this._values.Clone();
+    }
+}
