145437e47943b393e9fdb3e25c064a19b8f70886
hinshiba <105423175+hinshiba@users.noreply.github.com>
2026-03-11 20:28:24 +0900

    feat: syscallの復元機能の追加

--- PlusPim/Debuggers/PlusPimDbg/Instructions/SyscallInstruction.cs
+++ PlusPim/Debuggers/PlusPimDbg/Instructions/SyscallInstruction.cs
@@ -6,15 +6,22 @@ namespace PlusPim.Debuggers.PlusPimDbg.Instructions;
 internal class SyscallInstruction(int sourceLine): IInstruction {
     public int SourceLine => sourceLine;
 
+
+    private readonly Stack<SyscallCode> _history = new();
+    private readonly Stack<int> _prevReadInt = new();
+    private readonly Stack<(Address, byte[])> _prevReadString = new();
+
+
     public void Execute(ExecuteContext context) {
-        switch(context.Registers[RegisterID.V0]) {
-            case (int)SyscallCode.PrintInt:
+        SyscallCode code = (SyscallCode)context.Registers[RegisterID.V0];
+        switch(code) {
+            case SyscallCode.PrintInt:
                 context.Log($"Syscall: print_int {context.Registers[RegisterID.A0]}");
 
                 Console.WriteLine(context.Registers[RegisterID.A0]);
                 break;
 
-            case (int)SyscallCode.PrintString:
+            case SyscallCode.PrintString:
                 context.Log($"Syscall: print_string at address 0x{context.Registers[RegisterID.A0]:X8}");
 
                 Address readAddr = new(context.Registers[RegisterID.A0]);
@@ -26,9 +33,13 @@ internal class SyscallInstruction(int sourceLine): IInstruction {
                 Console.WriteLine(System.Text.Encoding.UTF8.GetString([.. bytes]));
                 break;
 
-            case (int)SyscallCode.ReadInt:
+            case SyscallCode.ReadInt:
                 context.Log("Syscall: read_int to $v0");
 
+                // Undoのために現在の値を保存
+                this._prevReadInt.Push(context.Registers[RegisterID.V0]);
+
+                // ユーザーからの入力を整数として読み取る
                 if(int.TryParse(Console.ReadLine(), out int value)) {
                     context.Registers[RegisterID.V0] = value;
                 } else {
@@ -37,13 +48,24 @@ internal class SyscallInstruction(int sourceLine): IInstruction {
                 }
                 break;
 
-            case (int)SyscallCode.ReadString:
+            case SyscallCode.ReadString:
                 context.Log($"Syscall: read_string to address 0x{context.Registers[RegisterID.A0]:X8}");
 
-                string input = Console.ReadLine() ?? "";
-                byte[] inputBytes = System.Text.Encoding.UTF8.GetBytes(input)[0..context.Registers[RegisterID.A1]];
-
                 Address writeAddr = new(context.Registers[RegisterID.A0]);
+                int maxLength = context.Registers[RegisterID.A1];
+
+                // Undoのためにメモリの内容を保存
+                List<byte> prevBytes = [];
+                for(int i = 0; i < maxLength + 1; i++) {
+                    prevBytes.Add(context.ReadMemoryByte(writeAddr + i));
+                }
+                this._prevReadString.Push((writeAddr, [.. prevBytes]));
+
+                // 実際に入力を受け取る
+                string input = Console.ReadLine() ?? "";
+                byte[] inputBytes = System.Text.Encoding.UTF8.GetBytes(input)[0..maxLength];
+
+
                 foreach(byte b in inputBytes) {
                     context.WriteMemoryByte(writeAddr, b);
                     writeAddr++;
@@ -51,7 +73,7 @@ internal class SyscallInstruction(int sourceLine): IInstruction {
                 context.WriteMemoryByte(writeAddr, 0); // null terminator
                 break;
 
-            case (int)SyscallCode.Exit:
+            case SyscallCode.Exit:
                 context.Log("Syscall: exit");
                 context.IsTerminated = true;
                 break;
@@ -60,12 +82,39 @@ internal class SyscallInstruction(int sourceLine): IInstruction {
                 context.Log($"Syscall: unknown code {context.Registers[RegisterID.V0]}");
                 break;
         }
+        this._history.Push(code);
     }
     public void Undo(ExecuteContext context) {
-        context.Log($"Syscall: print_int {context.Registers[RegisterID.A0]}");
+        switch(this._history.Pop()) {
+            case SyscallCode.PrintInt:
+            case SyscallCode.PrintString:
+                // 画面に出力した内容は消せないので無視
+                break;
+
+            case SyscallCode.ReadInt:
+                // レジスタの値の復元
+                context.Registers[RegisterID.V0] = this._prevReadInt.Pop();
+                break;
+
+            case SyscallCode.ReadString:
+                // メモリの内容の復元
+                (Address addr, byte[]? bytes) = this._prevReadString.Pop();
+                foreach(byte b in bytes) {
+                    context.WriteMemoryByte(addr++, b);
+                }
+                break;
+
+            case SyscallCode.Exit:
+                // フラグの復元
+                context.IsTerminated = false;
+                break;
+
+            default:
+                // 不明なコードは何もしていないので無視
+                break;
+        }
     }
 }
-
 
 internal enum SyscallCode {
     PrintInt = 1,
