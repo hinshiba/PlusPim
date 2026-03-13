d5f877c4d4abd4360527b0951790490b5d0fddbe
hinshiba <105423175+hinshiba@users.noreply.github.com>
2026-03-13 13:00:16 +0900

    add: メモリ操作命令基底クラスの実装

--- PlusPim/Debuggers/PlusPimDbg/Instructions/MemoryInstruction.cs
+++ PlusPim/Debuggers/PlusPimDbg/Instructions/MemoryInstruction.cs
@@ -13,14 +13,25 @@ internal abstract class MemoryInstruction: IInstruction {
     protected bool IsWrite { get; }
 
     /// <summary>
+    /// 符号拡張するかどうか <see cref="this.IsWrite"/>が<see langword="false"/>のときのみ意味を持つ
+    /// </summary>
+    protected bool IsSign { get; }
+
+    /// <summary>
     /// 操作するバイト数
     /// </summary>
     protected abstract int ByteNum { get; }
 
-    protected MemoryInstruction(RegisterID rt, Address addr, bool isWrite, int sourceLine) {
+    /// <summary>
+    /// 逆操作のためのスタック．書き込み命令なら元のメモリの値，読み込み命令なら元のレジスタの値を保存する
+    /// </summary>
+    protected Stack<uint> PrevNum = new();
+
+    protected MemoryInstruction(RegisterID rt, Address addr, bool isWrite, bool isSign, int sourceLine) {
         this.Rt = rt;
         this.Addr = addr;
         this.IsWrite = isWrite;
+        this.IsSign = isSign;
         this.SourceLine = sourceLine;
     }
 
@@ -39,22 +50,32 @@ internal abstract class MemoryInstruction: IInstruction {
     }
 
     private void ExecuteWrite(ExecuteContext context) {
-        context.Log($"Memory Write: 0x{this.Addr:X8} <= {context.Registers[this.Rt]} (ByteNum: {this.ByteNum})");
+        context.Log($"Memory Write: {this.Addr} <= {context.Registers[this.Rt]} (ByteNum: {this.ByteNum})");
 
         // Undoのために保存
-
+        this.PrevNum.Push(context.ReadMemoryBytes(this.Addr, this.ByteNum, false));
 
         // 書き込み
-        context.WriteMemoryByte(this.Addr, (byte)(context.Registers[this.Rt] & 0xFF));
+        context.WriteMemoryBytes(this.Addr, (uint)context.Registers[this.Rt], this.ByteNum);
 
     }
 
     private void ExecuteRead(ExecuteContext context) {
+        context.Log($"Memory Read: {this.Addr} => {this.Rt} (ByteNum: {this.ByteNum}, IsSign: {this.IsSign})");
 
+        // Undoのために保存
+        this.PrevNum.Push((uint)context.Registers[this.Rt]);
+
+        // 読み込み
+        context.Registers[this.Rt] = (int)context.ReadMemoryBytes(this.Addr, this.ByteNum, this.IsSign);
     }
 
 
     public void Undo(ExecuteContext context) {
-        throw new NotImplementedException();
+        if(this.IsWrite) {
+            context.WriteMemoryBytes(this.Addr, this.PrevNum.Pop(), this.ByteNum);
+        } else {
+            context.Registers[this.Rt] = (int)this.PrevNum.Pop();
+        }
     }
 }
